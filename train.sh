fbfba8ebce40a49f67d58ad79484177b4200fe7e
Ming Wu <mingwucn@outlook.com>
2025-03-24 23:13:49 +0100

    fix: update model name and log file in training script

--- train.sh
+++ train.sh
@@ -4,4 +4,4 @@
 mkdir -p log
 
 # Run the training command, print output to the screen, and append to log/train.txt with timestamps
-python ./trainer.py --epochs 50 --batch_size 2 --learning_rate 1e-5 --model_name 'SmartGrinding' --verbose_interval 10 2>&1 | tee >(while IFS= read -r line; do echo "$(date '+%Y-%m-%d %H:%M:%S') $line" >> log/train.txt; done)
+python ./trainer.py --epochs 50 --batch_size 2 --learning_rate 1e-5 --model_name 'SmartGrinding_all' --verbose_interval 10 2>&1 | tee >(while IFS= read -r line; do echo "$(date '+%Y-%m-%d %H:%M:%S') $line" >> log/train_all.txt; done)
