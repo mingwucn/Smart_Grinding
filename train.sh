5d55c95b833899cd0f9e805078a5ee26592e821a
Ming Wu <mingwucn@outlook.com>
2025-03-24 21:52:21 +0100

    sh and log

--- train.sh
+++ train.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Run the training command, print output to the screen, and append to log.txt with timestamps
-python ./trainer.py --epochs 50 --batch_size 2 --learning_rate 1e-5 --model_name 'SmartGrinding' 2>&1 | tee >(while IFS= read -r line; do echo "$(date '+%Y-%m-%d %H:%M:%S') $line" >> log.txt; done)
+# Create the log directory if it doesn't exist
+mkdir -p log
+
+# Run the training command, print output to the screen, and append to log/train.txt with timestamps
+python ./trainer.py --epochs 50 --batch_size 2 --learning_rate 1e-5 --model_name 'SmartGrinding' 2>&1 | tee >(while IFS= read -r line; do echo "$(date '+%Y-%m-%d %H:%M:%S') $line" >> log/train.txt; done)
