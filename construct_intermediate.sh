5e00b8371f446a7e185a81d1d582fd2e86bf816a
Ming Wu <mingwucn@outlook.com>
2025-04-19 04:35:04 +0200

    feat: add construct_intermediate.sh script for processing intermediate data

---
+++ construct_intermediate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p log
+
+intermediate_data_type=('physics' 'spec')
+
+for input_type in "${intermediate_data_type[@]}"; do
+    echo "Constructing intermediate data: $input_type"
+    python ./GrindingData.py --threads=6 --process_type "$input_type" 2>&1 | tee >(while IFS= read -r line; do echo "$(date '+%Y-%m-%d %H:%M:%S') $line" >>"log/preprocessing_${input_type}.txt"; done)
+done
